0f3d1c375b9d1a2a3eab9939af2ceeaa1c999794
jessicamhanna <jmhanna.ca@gmail.com>
2025-09-04 23:24:10 +0000

    engage: go live with Week 4 Greeting Generator

---
+++ w4/README.md
@@ -0,0 +1,3 @@
+# Week 4: Instant Greeting Generator
+
+This project is a simple interactive webpage that takes a user's name from a form input and displays a personalized greeting. It reinforces skills in HTML structure, form handling in JavaScript, and using Git/GitHub for version control.
--- w4/app.js
+++ w4/app.js
@@ -1,1 +1,20 @@
+// get the html ids
+const form = document.getElementById("greetingForm");
+const nameInput = document.getElementayById("nameInput");
+    
+// listen for the submit
+// stop default browser behavior
+form.addEventListener("submit", function(event) {eventListenerDefault();
 
+// get input
+const name = nameInput.value;
+
+// create greeting output
+const greeting = 'Greetings $[name}!';
+
+// show greeting output
+output.textContent = greeting; 
+
+// reset form
+form.requestFullscreen();
+});
